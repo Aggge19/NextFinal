ac39f1f8d895987123f3637c3424ac029be3ea33
Aggge19 <angelagcre19@gmail.com>
2022-01-25 17:18:46 +0100

    navigation modificado

--- components/navigation.js
+++ components/navigation.js
@@ -4,43 +4,32 @@ export default function navigation() {
     return (
 <nav className="navbar navbar-expand-lg navbar-light bg-light">
     <div className="container-fluid">
-        <a className="navbar-brand" href="">Málaga   </a>
+        <Link href="">
+            <a className="navbar-brand">Málaga</a>
+        </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
         </button>
     <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">
             <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                <Link href="/">
+                    <a className="nav-link active" aria-current="page">Index</a>
+                </Link>
             </li>
             <li className="nav-item">
-                <a className="nav-link" href="/restaurants">Restaurants</a>
+                <Link href="/restaurants">
+                    <a className="nav-link">Restaurants</a>
+                </Link>
             </li>
             <li className="nav-item">
-                <a className="nav-link" href="/reviews">Reviews</a>
+                <Link href="/reviews">
+                    <a className="nav-link">Reviews</a>
+                </Link>
             </li>
         </ul>
     </div>
     </div>
 </nav>
-        // <div>
-        //     <ul>
-        //         <li>
-        //             <Link href="/">
-        //                 <a>Index</a>
-        //             </Link>
-        //         </li>
-        //         <li>
-        //             <Link href="/restaurants">
-        //                 <a>Restaurants</a>
-        //             </Link>
-        //         </li>
-        //         <li>
-        //             <Link href="/reviews">
-        //                 <a>Reviews</a>
-        //             </Link>
-        //         </li>
-        //     </ul>
-        // </div>
     );
 }
